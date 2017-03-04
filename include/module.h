fae561211415c8641083a081b06ea03948d58bda
PublicHadyniak <public.hadyniak@gmail.com>
2017-03-04 15:49:29 +0200

    init

---
+++ include/module.h
@@ -0,0 +1,8 @@
+#ifndef MODULE_H
+#define MODULE_H
+
+// paste your module interface here
+
+void Module_print(void);
+
+#endif
---
+++ tests/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <check.h>
+#include <module.h>
+
+START_TEST (test_sample)
+{
+    ck_assert(1);
+}
+END_TEST
+
+START_TEST (test_sample_2)
+{
+    ck_assert(0);
+}
+END_TEST
+
+Suite *test_suite() {
+  Suite *s = suite_create("Module");
+  TCase *tc_sample;
+
+  tc_sample = tcase_create("TestCase");
+  tcase_add_test(tc_sample, test_sample);
+  tcase_add_test(tc_sample, test_sample_2);
+
+  suite_add_tcase(s, tc_sample);
+
+  return s;
+}
+
+int main() {
+  Suite *s = test_suite();
+  SRunner *sr = srunner_create(s);
+  srunner_set_fork_status(sr, CK_NOFORK);  // important for debugging!
+
+  srunner_run_all(sr, CK_VERBOSE);
+
+  int num_tests_failed = srunner_ntests_failed(sr);
+  srunner_free(sr);
+  return num_tests_failed;
+}
